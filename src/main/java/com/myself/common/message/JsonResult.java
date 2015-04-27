036985b86a3e2aabd88da7f0337d4ef754cbe650
hes <heshengyx@qq.com>
2015-04-27 18:19:48 +0800

    9

---
+++ src/main/java/com/myself/common/message/JsonResult.java
@@ -0,0 +1,36 @@
+package com.myself.common.message;
+
+import java.util.List;
+
+public class JsonResult<T> {
+
+	private int sEcho;
+	private int iTotalRecords;
+	private int iTotalDisplayRecords;
+	private List<T> aaData;
+	
+	public int getsEcho() {
+		return sEcho;
+	}
+	public void setsEcho(int sEcho) {
+		this.sEcho = sEcho;
+	}
+	public int getiTotalRecords() {
+		return iTotalRecords;
+	}
+	public void setiTotalRecords(int iTotalRecords) {
+		this.iTotalRecords = iTotalRecords;
+	}
+	public int getiTotalDisplayRecords() {
+		return iTotalDisplayRecords;
+	}
+	public void setiTotalDisplayRecords(int iTotalDisplayRecords) {
+		this.iTotalDisplayRecords = iTotalDisplayRecords;
+	}
+	public List<T> getAaData() {
+		return aaData;
+	}
+	public void setAaData(List<T> aaData) {
+		this.aaData = aaData;
+	}
+}
