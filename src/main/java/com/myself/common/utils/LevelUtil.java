d496059cc0b757f888e61b8f10f5c4b8211f7ebc
hes <heshengyx@qq.com>
2015-05-06 18:21:24 +0800

    1

---
+++ src/main/java/com/myself/common/utils/LevelUtil.java
@@ -0,0 +1,9 @@
+package com.myself.common.utils;
+
+public class LevelUtil {
+
+	public static final String LEVEL_PROVINCE = "1";
+	public static final String LEVEL_CITY = "2";
+	public static final String LEVEL_DISTRICT = "3";
+	public static final String LEVEL_TOWN = "4";
+}
